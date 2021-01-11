3c40fe12eef647e972113e7069070bdb92d135df
MJbae <bmj5208@gmail.com>
2021-01-11 14:38:23 +0900

    Add: Node 클래스 초기형태 구현

--- src/main/java/MJ/LinkedList/Node.java
+++ src/main/java/MJ/LinkedList/Node.java
@@ -1,4 +1,17 @@
 package MJ.LinkedList;
 
 public class Node {
+    private String _title;
+    private Node next = null;
+
+    // id는 추후 구현 생성자에 random 함수 활용
+    // private String id;
+
+    Node(String title){
+        this._title = title;
+    }
+
+    public String get_title() {
+        return _title;
+    }
 }
